6afec43954e7ea8a0b09329328796a90b5834acf
birendrajha <birendrajha1500123@gmail.com>
2020-12-05 18:54:54 +0530

    lets do it

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,43 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [num,setTime] = useState(0)
+  const [start,setStart] = useState(false)
+//  countdown()=>{
+//    if(time>0){
+//            const id = setInterval(()=>{
+//              time = time-1;
+//              setTime(time);
+//            },1000)
+//           }
+//  }
 
+function countdown(event){
+  let num = parseInt(event.target.value);
+  setStart(true);
+ num =  Math.floor(num)
+  document.getElementById("current-time").innerText=num;
+  if(num>0){
+                const id = setInterval(()=>{
+                num = num-1;
+                  setTime(num);
+                },1000)
+               }
+}
+  
+
+    
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={countdown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+    { start?
+      <div id="current-time">{num}</div>
+      :null
+    }
     </div>
   )
 }
